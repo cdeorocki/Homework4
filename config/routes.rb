49bc72e69981bedeca921eea1d3b2ee12834d430
Cecilly Deorocki <cdeorocki@live.com>
2015-12-16 21:18:37 -0500

    second commit, finished assignment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  get 'home' => 'home#index'
+  get 'contact' => 'home#index'
+
+  post 'contact-form' => 'home#contactForm'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
